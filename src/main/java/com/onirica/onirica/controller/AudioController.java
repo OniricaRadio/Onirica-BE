cc6fc9fdc18a30a887654c37d238013f74b28543
Karisha:) <krshmelendez@gmail.com>
2025-05-17 00:23:20 +0200

    fisrt commit

---
+++ src/main/java/com/onirica/onirica/controller/AudioController.java
@@ -0,0 +1,6 @@
+package com.onirica.onirica.controller;
+
+public class AudioController {
+    private String AudioSource = "/src/main/resources/audio" ;
+    
+}
---
+++ src/test/java/com/onirica/onirica/OniricaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onirica.onirica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OniricaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
