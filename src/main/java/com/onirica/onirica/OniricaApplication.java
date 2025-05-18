34d0b3438576f8d22cfb6103af24d9831574f5be
Karisha:) <krshmelendez@gmail.com>
2025-05-18 14:47:53 +0200

    created database connection structure

--- src/main/java/com/onirica/onirica/OniricaApplication.java
+++ src/main/java/com/onirica/onirica/OniricaApplication.java
@@ -3,10 +3,20 @@ package com.onirica.onirica;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class OniricaApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		System.setProperty("DB_URL", dotenv.get("DB_URL"));
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
+		SpringApplication.run(OniricaApplication.class, args);
+
+		System.out.println("Connected Successfully");
 		SpringApplication.run(OniricaApplication.class, args);
 	}
 
